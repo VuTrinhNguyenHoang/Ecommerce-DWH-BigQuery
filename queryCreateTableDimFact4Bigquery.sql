f01d4b4524452d616af194f90902c62ffc039bd4
Banmkh6 <banmkh6@gmail.com>
2025-05-12 19:52:11 +0700

    add queryCreateTableDimFact4Bigquery.sql file

---
+++ queryCreateTableDimFact4Bigquery.sql
@@ -0,0 +1,51 @@
+CREATE TABLE your_dataset_name.dim_products (
+    product_id STRING,
+    product_name STRING,
+    product_description STRING,
+    product_inventory_status STRING,
+    effective_date DATE,
+    end_date DATE,
+    is_active BOOLEAN
+) 
+PARTITION BY effective_date;
+
+CREATE TABLE your_dataset_name.fact_products (
+  product_id STRING,
+  snapshot_date DATE,
+  product_all_time_quantity_sold INT64,
+  product_stock_item_qty INT64,
+  product_stock_item_max_sale_qty INT64,
+  product_price FLOAT64,
+  product_list_price FLOAT64,
+  product_discount FLOAT64,
+  product_discount_rate FLOAT64
+)
+PARTITION BY snapshot_date;
+
+
+CREATE TABLE your_dataset_name.dim_comments (
+    comment_id STRING,
+    product_id STRING,
+    sentiment STRING,
+    comment_content STRING,
+    comment_rating INT64,
+    created_at TIMESTAMP,
+    purchased_at TIMESTAMP,
+    days_to_review INT64,
+    rating_valid BOOLEAN,
+    effective_date DATE,
+    end_date DATE,
+    is_active BOOLEAN
+) 
+PARTITION BY effective_date
+CLUSTER BY product_id;
+
+CREATE TABLE your_dataset_name.fact_comments (
+  comment_id STRING NOT NULL,
+  product_id STRING,
+  days_to_review INT64,
+  comment_rating INT64,
+  comment_date DATE,
+  snapshot_date DATE
+)
+PARTITION BY comment_date;
